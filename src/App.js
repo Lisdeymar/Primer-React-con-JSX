87a1af9a98c0b06ed304de9a56edb34f7e0f80f5
Lisdeymar <lisdeymar07@gmail.com>
2022-05-02 10:41:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Tasklist } from './components/Tasklist';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <Tasklist />
+     
+    </div>
+  );
+}
+
+export default App;
